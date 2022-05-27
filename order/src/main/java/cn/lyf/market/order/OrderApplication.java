8b8f36fcfa23225a66d62250a1280759d68f524e
yfl <stfujnkklyf@gmail.com>
2022-05-27 22:34:16 +0800

    sub_mod

---
+++ order/src/main/java/cn/lyf/market/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package cn.lyf.market.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}

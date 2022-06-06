2b9543db0d326fa1f3270335027ba69f293ca313
stfujnkk <3365310020@qq.com>
2022-06-06 18:21:56 +0800

    迁移后更新配置

--- renren-fast/src/main/java/io/renren/RenrenApplication.java
+++ renren-fast/src/main/java/io/renren/RenrenApplication.java
@@ -9,6 +9,7 @@
 package io.renren;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 

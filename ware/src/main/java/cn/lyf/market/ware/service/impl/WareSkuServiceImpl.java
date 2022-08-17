c01f32862f8c771d6911ebff5a4a90bc2e33b14a
stfujnkk <3365310020@qq.com>
2022-08-17 22:36:39 +0800

    feat(ware):查询库存,创建采购单

--- ware/src/main/java/cn/lyf/market/ware/service/impl/WareSkuServiceImpl.java
+++ ware/src/main/java/cn/lyf/market/ware/service/impl/WareSkuServiceImpl.java
@@ -1,7 +1,10 @@
 package cn.lyf.market.ware.service.impl;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -18,11 +21,19 @@ public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> i
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<WareSkuEntity> wrapper = new QueryWrapper<>();
+        String skuId = (String) params.get("skuId");
+        if (!StringUtils.isEmpty(skuId)) {
+            wrapper.eq("sku_id", skuId);
+        }
+        String wareId = (String) params.get("wareId");
+        if (!StringUtils.isEmpty(wareId)) {
+            wrapper.eq("ware_id", wareId);
+        }
         IPage<WareSkuEntity> page = this.page(
                 new Query<WareSkuEntity>().getPage(params),
-                new QueryWrapper<WareSkuEntity>()
+                wrapper
         );
-
         return new PageUtils(page);
     }
 

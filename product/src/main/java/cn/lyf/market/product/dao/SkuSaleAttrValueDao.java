44f122d00504a2e70d89a9ed127098d12fff46d6
stfujnkk <3365310020@qq.com>
2023-02-18 22:11:31 +0800

    feat: sku组合切换及跳转

--- product/src/main/java/cn/lyf/market/product/dao/SkuSaleAttrValueDao.java
+++ product/src/main/java/cn/lyf/market/product/dao/SkuSaleAttrValueDao.java
@@ -3,13 +3,14 @@ package cn.lyf.market.product.dao;
 import cn.lyf.market.product.entity.SkuSaleAttrValueEntity;
 import cn.lyf.market.product.vo.ItemSaleAttrVo;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 /**
  * sku销售属性&值
- * 
+ *
  * @author lyf
  * @email 3365310020@qq.com
  * @date 2022-05-28 12:18:19
@@ -17,5 +18,5 @@ import java.util.List;
 @Mapper
 public interface SkuSaleAttrValueDao extends BaseMapper<SkuSaleAttrValueEntity> {
 
-	List<ItemSaleAttrVo> getSaleAttrsBySpuId(Long spuId);
+	List<ItemSaleAttrVo> getSaleAttrsBySpuId(@Param("spuId") Long spuId);
 }
--- product/src/main/java/cn/lyf/market/product/vo/ItemSaleAttrVo.java
+++ product/src/main/java/cn/lyf/market/product/vo/ItemSaleAttrVo.java
@@ -2,9 +2,17 @@ package cn.lyf.market.product.vo;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class ItemSaleAttrVo {
 	private Long attrId;
 	private String attrName;
-	private String attrValues;
+	private List<AttrValue> attrValues;
+
+	@Data
+	public static class AttrValue {
+		private String attrValue;
+		private String skuIds;
+	}
 }
